e45e2951d81badad4ee3cd48707840b9c28c56e6
angelinapoltavskiy <angelinapoltavskiy@gmail.com>
2022-05-15 00:46:23 -0700

    fixing positioning

--- js/index.js
+++ js/index.js
@@ -1,19 +1,106 @@
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() { myHeader() };
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() { myHeader() };
 
-// Get the header
-var header = document.getElementById("myHeader");
+// // Get the header
+// var header = document.getElementById("myHeader");
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function header() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function header() {
+//     if (window.pageYOffset > sticky) {
+//         header.classList.add("sticky");
+//     } else {
+//         header.classList.remove("sticky");
+//     }
+// }
+
+
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
     }
-}
+  }
+  
 
+
+
+
+//   window.addEventListener("scroll", reveal);
+
+
+//   window.addEventListener(
+//     'scroll',
+//     function() {
+//       if (window.scrollY > introHeight) {
+//         $topButton.fadeIn();
+//       } else {
+//         $topButton.fadeOut();
+//       }
+//     },
+//     false
+//   );
+
+//   topButton.addEventListener('click', function() {
+//     $('html, body').animate({ scrollTop: 0 }, 500);
+//   });
+
+
+
+
+// gsap.registerPlugin(ScrollTrigger);
+// // REVEAL //
+// gsap.utils.toArray(".revealUp").forEach(function (elem) {
+//   ScrollTrigger.create({
+//     trigger: elem,
+//     start: "top 80%",
+//     end: "bottom 20%",
+//     markers: true,
+//     onEnter: function () {
+//       gsap.fromTo(
+//         elem,
+//         { y: 100, autoAlpha: 0 },
+//         {
+//           duration: 1.25,
+//           y: 0,
+//           autoAlpha: 1,
+//           ease: "back",
+//           overwrite: "auto"
+//         }
+//       );
+//     },
+//     onLeave: function () {
+//       gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+//     },
+//     onEnterBack: function () {
+//       gsap.fromTo(
+//         elem,
+//         { y: -100, autoAlpha: 0 },
+//         {
+//           duration: 1.25,
+//           y: 0,
+//           autoAlpha: 1,
+//           ease: "back",
+//           overwrite: "auto"
+//         }
+//       );
+//     },
+//     onLeaveBack: function () {
+//       gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+//     }
+//   });
+// });
